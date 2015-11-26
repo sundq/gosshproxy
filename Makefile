fe6f5bdbc90cd7776bf7801b204f1e76401de140
sundq <tell88me@gmail.com>
2015-11-26 16:08:35 +0800

    implement sshproxy

--- Makefile
+++ Makefile
@@ -1,56 +1,15 @@
 OUTPUT_DIR = ./builds
 
-gosshproxy: app/resource.go main.go init_main.go
-	go build
-
-resource:  app/resource.go
-
-app/resource.go: bindata/static/js/hterm.js bindata/static/js/gotty.js  bindata/static/index.html bindata/static/favicon.png
-	go-bindata -prefix bindata -pkg app -ignore=\\.gitkeep -o app/resource.go bindata/...
-	gofmt -w app/resource.go
-
-bindata:
-	mkdir bindata
-
-bindata/static: bindata
-	mkdir bindata/static
-
-bindata/static/index.html: bindata/static resources/index.html
-	cp resources/index.html bindata/static/index.html
-
-bindata/static/favicon.png: bindata/static resources/favicon.png
-	cp resources/favicon.png bindata/static/favicon.png
-
-bindata/static/js: bindata/static
-	mkdir -p bindata/static/js
-
-bindata/static/js/hterm.js: bindata/static/js libapps/hterm/js/*.js
-	cd libapps && \
-	LIBDOT_SEARCH_PATH=`pwd` ./libdot/bin/concat.sh -i ./hterm/concat/hterm_all.concat -o ../bindata/static/js/hterm.js
-
-bindata/static/js/gotty.js: bindata/static/js resources/gotty.js
-	cp resources/gotty.js bindata/static/js/gotty.js
+dbsshproxy: main.go readers.go sshproxy.go 
+	go build -o dbsshproxy
 
 tools:
 	go get github.com/tools/godep
 	go get github.com/mitchellh/gox
 	go get github.com/tcnksm/ghr
 
-deps:
-	godep restore
-
-test:
-	if [ `go fmt ./... | wc -l` -gt 0 ]; then echo "go fmt error"; exit 1; fi
-
 cross_compile:
-	GOARM=5 gox -os="darwin linux freebsd netbsd openbsd" -arch="386 amd64 arm" -output "${OUTPUT_DIR}/pkg/{{.OS}}_{{.Arch}}/{{.Dir}}"
-
-targz:
-	mkdir -p ${OUTPUT_DIR}/dist
-	cd ${OUTPUT_DIR}/pkg/; for osarch in *; do (cd $$osarch; tar zcvf ../../dist/gotty_$$osarch.tar.gz ./*); done;
+	GOARM=5 gox -os="darwin linux" -arch="386 amd64" -output "${OUTPUT_DIR}/pkg/{{.OS}}_{{.Arch}}/{{.Dir}}"
 
 shasums:
 	cd ${OUTPUT_DIR}/dist; shasum * > ./SHASUMS
-
-release:
-	ghr --delete --prerelease -u yudai -r gotty pre-release ${OUTPUT_DIR}/dist
--- main.go
+++ main.go
@@ -2,40 +2,138 @@ package main
 
 import (
 	"./libs"
-	"./sshproxy"
-	"./webtty"
-	"github.com/codegangsta/cli"
-	"log"
-	"os"
-	"sync"
+	. "./log"
+	"code.google.com/p/go.crypto/ssh"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
+	"strings"
+	// "sync"
 )
 
+type code_info struct {
+	code    string
+	peer_ip string
+	way     string
+}
+
+func get_user_name(user string) (string, string, int, string, error) {
+	tmp_user := strings.Split(user, "_")
+	if len(tmp_user) < 2 {
+		return "", "", 0, "", errors.New("User does not exist")
+	}
+
+	code_info, err := libs.GetCodeInfo(tmp_user[0])
+	if err != nil {
+		return "", "", 0, "", err
+	}
+
+	target_user := strings.Join(tmp_user[1:len(tmp_user)], "_")
+	target_ip, _ := code_info.Get("resource").Get("ip_address").String()
+	target_port, _ := code_info.Get("port").Int()
+
+	return target_user, target_ip, target_port, tmp_user[0], nil
+}
+
 func main() {
-	cmd := cli.NewApp()
-	// cmd.Version = Version
-	cmd.Usage = "Share your terminal as a web application"
-	cmd.HideHelp = true
-
-	flags := []cli.Flag{
-		cli.StringFlag{Name: "key", Value: "", Usage: "agent key"},
-		cli.StringFlag{Name: "hostname", Value: "", Usage: "hostname of agent"},
-	}
-	cmd.Flags = flags
-
-	cmd.Action = func(c *cli.Context) {
-		libs.SetConfig(c.String("key"), c.String("hostname"), c.String("ssh_private_key"))
-		InitMain()
-		cc, err := libs.NewCenterCommunication("./diaobaoyun.sock")
-		if err != nil {
-			log.Printf("Create uinx domain sock failed")
-			return
-		}
-		var wg sync.WaitGroup
-		wg.Add(2)
-		go webtty.WettyMain(&wg, cc)
-		go sshproxy.SshProxyMain(&wg, cc)
-		wg.Wait()
+	db_config, err := libs.GetConfig()
+	listen := fmt.Sprintf(":%d", db_config.SshPort)
+	key := db_config.SshPrivateKey
+	cc, err := libs.NewCenterCommunication("/Users/sundq/workspace/pentagon/diaobaoyun.sock")
+	if err != nil {
+		Log.Error("Create uinx domain sock failed")
+		return
 	}
 
-	cmd.Run(os.Args)
+	privateBytes, err := ioutil.ReadFile(key)
+	if err != nil {
+		panic("Failed to load private key")
+	}
+
+	private, err := ssh.ParsePrivateKey(privateBytes)
+	if err != nil {
+		panic("Failed to parse private key")
+	}
+
+	var sessions map[net.Addr]map[string]interface{} = make(map[net.Addr]map[string]interface{})
+
+	config := &ssh.ServerConfig{
+		PasswordCallback: func(c ssh.ConnMetadata, pass []byte) (*ssh.Permissions, error) {
+			fmt.Printf("Login attempt: %s, user %s", c.RemoteAddr(), c.User())
+
+			sessions[c.RemoteAddr()] = map[string]interface{}{
+				"username": c.User(),
+				"password": string(pass),
+			}
+			user, ip, port, code, err := get_user_name(c.User())
+			if err != nil {
+				return nil, err
+			}
+			clientConfig := &ssh.ClientConfig{}
+			clientConfig.User = user
+			clientConfig.Auth = []ssh.AuthMethod{
+				// ssh.PublicKeys(signer),
+				ssh.Password(string(pass)),
+			}
+			Log.Info("connect to dest:", fmt.Sprintf("%s:%d", ip, port), err)
+			client, err := ssh.Dial("tcp", fmt.Sprintf("%s:%d", ip, port), clientConfig)
+			sessions[c.RemoteAddr()]["client"] = client
+			sessions[c.RemoteAddr()]["code"] = &code_info{
+				code:    code,
+				way:     "terminal",
+				peer_ip: c.RemoteAddr().String(),
+			}
+			return nil, err
+		},
+		PublicKeyCallback: func(c ssh.ConnMetadata, key ssh.PublicKey) (*ssh.Permissions, error) {
+			fmt.Printf("Login attempt: %s, user %s", c.RemoteAddr(), c.User())
+
+			sessions[c.RemoteAddr()] = map[string]interface{}{
+				"username": c.User(),
+				"password": string(key.Marshal()),
+			}
+			user, ip, port, code, err := get_user_name(c.User())
+			if err != nil {
+				return nil, err
+			}
+			clientConfig := &ssh.ClientConfig{}
+			clientConfig.User = user
+			signer, _ := ssh.ParsePrivateKey(privateBytes)
+			clientConfig.Auth = []ssh.AuthMethod{
+				ssh.PublicKeys(signer),
+				// ssh.Password(string(pass)),
+			}
+			Log.Info("connect to dest:", fmt.Sprintf("%s:%d", ip, port), err)
+			client, err := ssh.Dial("tcp", fmt.Sprintf("%s:%d", ip, port), clientConfig)
+			sessions[c.RemoteAddr()]["client"] = client
+			sessions[c.RemoteAddr()]["code"] = &code_info{
+				code:    code,
+				way:     "terminal",
+				peer_ip: c.RemoteAddr().String(),
+			}
+			return nil, err
+		},
+	}
+
+	config.AddHostKey(private)
+
+	ListenAndServe(listen, config, cc, func(c ssh.ConnMetadata) (*ssh.Client, error) {
+		meta, _ := sessions[c.RemoteAddr()]
+		client := meta["client"].(*ssh.Client)
+		Log.Info("Connection accepted from: %s", c.RemoteAddr())
+
+		return client, err
+	}, func(c ssh.ConnMetadata) (*code_info, error) {
+		meta, _ := sessions[c.RemoteAddr()]
+		code_info := meta["code"].(*code_info)
+		Log.Info("code info: %s", code_info)
+		return code_info, nil
+	}, func(c ssh.ConnMetadata, r io.ReadCloser) (io.ReadCloser, error) {
+		return NewTypeWriterReadCloser(r), nil
+	}, func(c ssh.ConnMetadata) error {
+		Log.Info("Connection closed.")
+		return nil
+	})
 }
